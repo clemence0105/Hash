f3f2b3e4cc8618985515fc65d7db1f880ed7b5f2
Darya Zernova <clemence7@mail.ru>
2015-06-05 21:47:06 +0700

    Initial commit

---
+++ gost341194.cpp
@@ -0,0 +1,276 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "funcs.h"
+#include <iostream>
+#include <string.h>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+string gost341194(byte* m, size_t msgLen);
+
+void F(byte* Hin, byte* m);
+void cntrlSum(byte* sum, byte* m);
+void xor32bytes(byte* a, byte* b);
+byte* roundKeys(byte* U, byte* V);
+void A(byte* Y);
+void P(byte* Y);
+void E(byte* D, byte* K);
+byte* stepE(byte* A, byte* K);
+void fiN(byte* Y, int n);
+
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "gost341194 <file_name>" << endl;
+		return 1;
+	}
+    int fileSize = 0;
+	byte* buf = readFile(argv[1], fileSize);
+	cout << gost341194(buf, fileSize) << endl;
+	delete[] buf;
+	return 0;
+}
+
+string gost341194(byte* m, size_t msgLen)
+{
+	byte h[32];
+	byte sum[32];
+	byte L[32];
+	memset(h, 0, 32);
+	memset(sum, 0, 32);
+	memset(L, 0, 32);
+
+	for (int i = 0; i < (int)msgLen - 31; i += 32)
+	{
+		byte* chunk = m + i;
+		cntrlSum(sum, chunk);
+		F(h, chunk);
+	}
+	L[msgLen / 32] = 1;
+
+	if (msgLen % 32)
+	{
+		L[0] = (msgLen % 32) * 8;
+
+		byte chunk[32];
+		memset(chunk, 0, 32);
+		memcpy(chunk, m + (msgLen / 32) * 32, msgLen % 32);
+		F(h, chunk);
+		cntrlSum(sum, chunk);
+	}
+
+	F(h, L);
+	F(h, sum);
+
+	ostringstream res;
+	res.fill('0');
+	for (int i = 0; i < 32; i++)
+		res << hex << setw(2) << (int)h[i];
+
+	char resString[65];
+	strcpy(resString, res.str().c_str());
+	return resString;
+}
+
+void F(byte* Hin, byte* m)
+{
+	byte* keys = roundKeys(Hin, m);
+
+	byte S[32];
+	for (int i = 0; i < 4; i++)
+	{
+		byte tmp[8];
+		memcpy(tmp, Hin + i * 8, 8);
+		E(tmp, keys + i*32);
+		memcpy(S + i*8, tmp, 8);
+	}
+	delete[] keys;
+
+	fiN(S, 12);
+	xor32bytes(S, m);
+	fiN(S, 1);
+	xor32bytes(Hin, S);
+	fiN(Hin, 61);
+}
+
+void cntrlSum(byte* sum, byte* m)
+{
+	int c = 0;
+	for (int i = 0; i < 32; i++)
+	{
+		c += sum[i] + m[i];
+		sum[i] = c & 0xFF;
+		c >>= 8;
+	}
+}
+
+void xor32bytes(byte* a, byte* b)
+{
+	for (size_t i = 0; i < 32; i++)
+		a[i] ^= b[i];
+}
+
+byte* roundKeys(byte* Uc, byte* Vc)
+{
+	byte U[32];
+	byte V[32];
+	memcpy(U, Uc, 32);
+	memcpy(V, Vc, 32);
+
+	byte C3[] = {
+		0x00, 0xff, 0x00, 0xff, 0x00, 0xff, 0x00, 0xff,
+		0xff, 0x00, 0xff, 0x00, 0xff, 0x00, 0xff, 0x00,
+		0x00, 0xff, 0xff, 0x00, 0xff, 0x00, 0x00, 0xff,
+		0xff, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0xff };
+
+	byte* K = new byte[4 * 32];
+	byte W[32];
+	memcpy(W, V, 32);
+	xor32bytes(W, U);
+	P(W);
+	memcpy(K, W, 32);
+	for (int j = 1; j < 4; j++)
+	{
+		A(U);
+		if (j == 2)
+			xor32bytes(U, C3);
+		A(V);
+		A(V);
+		memcpy(W, V, 32);
+		xor32bytes(W, U);
+		P(W);
+		memcpy(K + j * 32, W, 32);
+	}
+
+	return K;
+}
+
+void A(byte* Y)
+{
+	byte res[32];
+	for (int i = 0; i < 24; i++)
+		res[i] = Y[i + 8];
+
+	for (int i = 0; i < 8; i++)
+		res[i + 24] = Y[i] ^ Y[i + 8];
+
+	memcpy(Y, res, 32);
+}
+
+void P(byte* Y)
+{
+	byte res[32];
+	for (int i = 0; i <= 3; i++)
+	for (int k = 1; k <= 8; k++)
+		res[i + 1 + 4 * (k - 1) - 1] = Y[8 * i + k - 1];
+
+	memcpy(Y, res, 32);
+}
+
+void E(byte* D, byte* K)
+{
+	byte A[4];
+	byte B[4];
+	memcpy(A, D, 4);
+	memcpy(B, D + 4, 4);
+
+	for (int j = 0; j < 3; j++)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			byte* tmp = stepE(A, K + i*4);
+			for (int i = 0; i < 4; i++)
+				tmp[i] ^= B[i];
+			memcpy(B, A, 4);
+			memcpy(A, tmp, 4);
+			delete[] tmp;
+		}
+	}
+
+	for (int i = 7; i >= 0; i--)
+	{
+		byte* tmp = stepE(A, K + i * 4);
+		for (int i = 0; i < 4; i++)
+			tmp[i] ^= B[i];
+		memcpy(B, A, 4);
+		memcpy(A, tmp, 4);
+		delete[] tmp;
+	}
+
+	memcpy(D, B, 4);
+	memcpy(D + 4, A, 4);
+}
+
+byte* stepE(byte* A, byte* K)
+{
+	unsigned char S[8][16] = {
+		{ 4, 10, 9, 2, 13, 8, 0, 14, 6, 11, 1, 12, 7, 15, 5, 3 },
+		{ 14, 11, 4, 12, 6, 13, 15, 10, 2, 3, 8, 1, 0, 7, 5, 9 },
+		{ 5, 8, 1, 13, 10, 3, 4, 2, 14, 15, 12, 7, 6, 0, 9, 11 },
+		{ 7, 13, 10, 1, 0, 8, 9, 15, 14, 4, 6, 12, 11, 2, 5, 3 },
+		{ 6, 12, 7, 1, 5, 15, 13, 8, 4, 10, 9, 14, 0, 3, 11, 2 },
+		{ 4, 11, 10, 0, 7, 2, 1, 13, 3, 6, 8, 5, 9, 12, 15, 14 },
+		{ 13, 11, 4, 1, 3, 15, 5, 9, 0, 10, 14, 7, 6, 8, 2, 12 },
+		{ 1, 15, 13, 0, 5, 7, 10, 4, 9, 2, 3, 14, 6, 11, 8, 12 },
+	};
+
+	byte* res = new byte[4];
+	int c = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		c += A[i] + K[i];
+		res[i] = c & 0xFF;
+		c >>= 8;
+	}
+
+	for (int i = 0; i < 8; i++)
+	{
+		int x = res[i >> 1] & ((i & 1) ? 0xF0 : 0x0F);
+
+		res[i >> 1] ^= x;
+		x >>= (i & 1) ? 4 : 0;
+		x = S[i][x];
+		res[i >> 1] |= x << ((i & 1) ? 4 : 0);
+	}
+
+	int tmp = res[3];
+	res[3] = res[2];
+	res[2] = res[1];
+	res[1] = res[0];
+	res[0] = tmp;
+	tmp = res[0] >> 5;
+
+	for (int i = 1; i < 4; i++)
+	{
+		int nTmp = res[i] >> 5;
+		res[i] = (res[i] << 3) | tmp;
+		tmp = nTmp;
+	}
+
+	res[0] = (res[0] << 3) | tmp;
+	return res;
+}
+
+void fiN(byte* Y, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		unsigned char tmp[] = { 0, 0 };
+
+		char indexes[] = { 1, 2, 3, 4, 13, 16 };
+		for (int j = 0; j < sizeof(indexes); j++)
+		{
+			tmp[0] ^= Y[2 * (indexes[j] - 1)];
+			tmp[1] ^= Y[2 * (indexes[j] - 1) + 1];
+		}
+
+		for (int i = 0; i < 30; i++)
+			Y[i] = Y[i + 2];
+
+		Y[30] = tmp[0];
+		Y[31] = tmp[1];
+	}
+}
---
+++ ripemd160.cpp
@@ -0,0 +1,203 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "funcs.h"
+#include <iostream>
+#include <string.h>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+string ripemd160(byte* m, size_t msgLen);
+dword f(int j, dword x, dword y, dword z);
+dword K1(int j);
+dword K2(int j);
+
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "ripemd160 <file_name>" << endl;
+		return 1;
+	}
+    int fileSize = 0;
+	byte* buf = readFile(argv[1], fileSize);
+	cout << ripemd160(buf, fileSize) << endl;
+	delete[] buf;
+	return 0;
+}
+
+string ripemd160(byte* m, size_t msgLen)
+{
+	// Шаг 1. Добавление недостающих битов
+	int zeroCount = 56 - (msgLen + 1) % 64;
+	if (zeroCount < 0)
+		zeroCount += 64;
+	
+	size_t newLen = msgLen + zeroCount; 
+	byte* msg = new byte[newLen + 1 + 8]();
+	memset(msg, 0, newLen);
+	memcpy(msg, m, msgLen);
+	msg[msgLen] = 0x80;
+	newLen++;
+
+
+	// Шаг 2. Добавление длины сообщения
+	int L = msgLen * 8;
+	for (int i = 0; i < 4; i++, newLen++)
+		msg[newLen] = L >> i * 8;
+	newLen += 4;
+
+
+	// Шаг 3. Определение действующих функций и констант
+	dword R1[] = {
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+		7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8,
+		3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12,
+		1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2,
+		4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13
+	};
+
+	dword R2[] = {
+		5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12,
+		6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2,
+		15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13,
+		8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14,
+		12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11
+	};
+
+	dword S1[] = {
+		11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8,
+		7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12,
+		11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5,
+		11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12,
+		9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6
+	};
+
+	dword S2[] = {
+		8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6,
+		9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11,
+		9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5,
+		15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8,
+		8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11
+	};
+
+	dword h[5];
+	h[0] = 0x67452301;
+	h[1] = 0xEFCDAB89;
+	h[2] = 0x98BADCFE;
+	h[3] = 0x10325476;
+	h[4] = 0xC3D2E1F0;
+
+	// объединяем байты в блоки по 32 бита
+	dword* blocks = (dword*)msg;
+	size_t blocksLen = newLen / 4;
+
+
+	// Шаг 4. Выполнение алгоритма хеширования
+	for (size_t i = 0; i < blocksLen; i += 16)
+	{
+		dword* x = blocks + i;
+
+		dword A1 = h[0];
+		dword B1 = h[1];
+		dword C1 = h[2];
+		dword D1 = h[3];
+		dword E1 = h[4];
+
+		dword A2 = h[0];
+		dword B2 = h[1];
+		dword C2 = h[2];
+		dword D2 = h[3];
+		dword E2 = h[4];
+
+		unsigned long T;
+
+		for (int j = 0; j < 80; j++)
+		{
+			T = ROL(A1 + f(j, B1, C1, D1) + x[R1[j]] + K1(j), S1[j]) + E1;
+			A1 = E1;
+			E1 = D1;
+			D1 = ROL(C1, 10);
+			C1 = B1;
+			B1 = T;
+
+			T = ROL(A2 + f(79 - j, B2, C2, D2) + x[R2[j]] + K2(j), S2[j]) + E2;
+			A2 = E2;
+			E2 = D2;
+			D2 = ROL(C2, 10);
+			C2 = B2;
+			B2 = T;
+		}
+
+		T = h[1] + C1 + D2;
+		h[1] = h[2] + D1 + E2;
+		h[2] = h[3] + E1 + A2;
+		h[3] = h[4] + A1 + B2;
+		h[4] = h[0] + B1 + C2;
+		h[0] = T;
+	}
+	delete[] msg;
+	ostringstream outStream;
+	outStream.fill('0');
+	for (int i = 0; i < 5; i++)
+		outStream << hex << setw(8) << bigToLittle(h[i]);
+
+	char resString[41];
+	strcpy(resString, outStream.str().c_str());
+	return resString;
+}
+
+dword f(int j, dword x, dword y, dword z)
+{
+	if (j < 16)
+		return x ^ y ^ z;
+
+	if (j < 32)
+		return (x & y) | (~x & z);
+
+	if (j < 48)
+		return (x | ~y) ^ z;
+
+	if (j < 64)
+		return (x & z) | (y & ~z);
+
+	if (j < 80)
+		return x ^ (y | ~z);
+}
+
+dword K1(int j)
+{
+	if (j < 16)
+		return 0x00000000;
+
+	if (j < 32)
+		return 0x5A827999;
+
+	if (j < 48)
+		return 0x6ED9EBA1;
+
+	if (j < 64)
+		return 0x8F1BBCDC;
+
+	if (j < 80)
+		return 0xA953FD4E;
+}
+
+dword K2(int j)
+{
+	if (j < 16)
+		return 0x50A28BE6;
+
+	if (j < 32)
+		return 0x5C4DD124;
+
+	if (j < 48)
+		return 0x6D703EF3;
+
+	if (j < 64)
+		return 0x7A6D76E9;
+
+	if (j < 80)
+		return 0x00000000;
+}
